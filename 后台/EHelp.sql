66f8567eca9c71192d553e5f5b34008a54cdd21c
linqiaonihao <824928207@qq.com>
2017-05-27 13:55:26 +0800

    求助添加经纬度

--- 后台/EHelp.sql
+++ 后台/EHelp.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.7.13, for osx10.11 (x86_64)
 --
--- Host: localhost    Database: Ehelp
+-- Host: 119.29.246.121    Database: EHelp
 -- ------------------------------------------------------
--- Server version	5.7.13
+-- Server version	5.7.18
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,179 +16,196 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table answer
+-- Table structure for table `answer`
 --
 
-DROP TABLE IF EXISTS answer;
-CREATE TABLE answer (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  answerer_id int(11) NOT NULL,
-  date datetime NOT NULL,
-  description varchar(200) NOT NULL,
-  question_id int(11) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `answer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `answer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `answerer_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table answer
+-- Dumping data for table `answer`
 --
 
-LOCK TABLES answer WRITE;
-/*!40000 ALTER TABLE answer DISABLE KEYS */;
-INSERT INTO answer VALUES (1,2,'2010-02-02 00:00:00','ads',3),(2,3,'2013-12-31 00:00:00','zxc',3);
-/*!40000 ALTER TABLE answer ENABLE KEYS */;
+LOCK TABLES `answer` WRITE;
+/*!40000 ALTER TABLE `answer` DISABLE KEYS */;
+INSERT INTO `answer` VALUES (1,2,'2010-02-02 00:00:00','ads',3),(2,3,'2013-12-31 00:00:00','zxc',3);
+/*!40000 ALTER TABLE `answer` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table contact
+-- Table structure for table `contact`
 --
 
-DROP TABLE IF EXISTS contact;
-CREATE TABLE contact (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  contact_phone varchar(45) NOT NULL,
-  contact_user varchar(45) NOT NULL,
-  user_id int(11) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contact_phone` varchar(45) NOT NULL,
+  `contact_user` varchar(45) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table contact
+-- Dumping data for table `contact`
 --
 
-LOCK TABLES contact WRITE;
-/*!40000 ALTER TABLE contact DISABLE KEYS */;
-INSERT INTO contact VALUES (1,'188','123',3),(2,'137','wer',3);
-/*!40000 ALTER TABLE contact ENABLE KEYS */;
+LOCK TABLES `contact` WRITE;
+/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+INSERT INTO `contact` VALUES (1,'188','123',3),(2,'137','wer',3),(3,'18819253762','林丹',3);
+/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table emergency
+-- Table structure for table `emergency`
 --
 
-DROP TABLE IF EXISTS emergency;
-CREATE TABLE emergency (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  date datetime NOT NULL,
-  finished int(11) NOT NULL,
-  launcher_id int(11) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `emergency`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `emergency` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `finished` int(11) NOT NULL,
+  `launcher_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table emergency
+-- Dumping data for table `emergency`
 --
 
-LOCK TABLES emergency WRITE;
-/*!40000 ALTER TABLE emergency DISABLE KEYS */;
-INSERT INTO emergency VALUES (1,'2018-02-02 00:00:00',1,3);
-/*!40000 ALTER TABLE emergency ENABLE KEYS */;
+LOCK TABLES `emergency` WRITE;
+/*!40000 ALTER TABLE `emergency` DISABLE KEYS */;
+INSERT INTO `emergency` VALUES (1,'2018-02-02 00:00:00',1,3);
+/*!40000 ALTER TABLE `emergency` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table help
+-- Table structure for table `help`
 --
 
-DROP TABLE IF EXISTS help;
-CREATE TABLE help (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  address varchar(200) NOT NULL,
-  date datetime NOT NULL,
-  description varchar(200) NOT NULL,
-  finished int(11) NOT NULL,
-  launcher_id int(11) NOT NULL,
-  title varchar(45) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `help`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `help` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `address` varchar(200) NOT NULL,
+  `date` datetime NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `finished` int(11) NOT NULL,
+  `launcher_id` int(11) NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `longitude` double NOT NULL,
+  `latitude` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table help
+-- Dumping data for table `help`
 --
 
-LOCK TABLES help WRITE;
-/*!40000 ALTER TABLE help DISABLE KEYS */;
-INSERT INTO help VALUES (1,'road','2017-09-21 00:00:00','help please',1,3,'help'),(2,'qwe','2013-02-12 00:00:00','asd',1,4,'qwe');
-/*!40000 ALTER TABLE help ENABLE KEYS */;
+LOCK TABLES `help` WRITE;
+/*!40000 ALTER TABLE `help` DISABLE KEYS */;
+INSERT INTO `help` VALUES (1,'road','2017-09-21 00:00:00','help please',1,3,'help',0,0),(2,'qwe','2013-02-12 00:00:00','asd',1,4,'qwe',0,0),(3,'guangzhou','2017-05-27 13:31:00','bangbangwo',0,3,'qiujiua',123.34,45.32),(4,'å¹¿å·','2017-05-27 13:34:00','bangbangwo',0,3,'qiujiua',123.34,45.32);
+/*!40000 ALTER TABLE `help` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table question
+-- Table structure for table `question`
 --
 
-DROP TABLE IF EXISTS question;
-CREATE TABLE question (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  asker_id int(11) NOT NULL,
-  date datetime NOT NULL,
-  description varchar(200) NOT NULL,
-  title varchar(45) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `asker_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `title` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table question
+-- Dumping data for table `question`
 --
 
-LOCK TABLES question WRITE;
-/*!40000 ALTER TABLE question DISABLE KEYS */;
-INSERT INTO question VALUES (3,3,'2020-02-02 00:00:00','zxc','zxzxc');
-/*!40000 ALTER TABLE question ENABLE KEYS */;
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (3,3,'2020-02-02 00:00:00','zxc','zxzxc'),(4,3,'2017-05-27 13:36:00','æä¹å­¦ä¹ æçä¼æ´é«','æä¹å­¦ä¹ '),(5,3,'2017-05-27 13:38:00','怎么学习效率会更高','怎么学习');
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table response
+-- Table structure for table `response`
 --
 
-DROP TABLE IF EXISTS response;
-CREATE TABLE response (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  event_id int(11) NOT NULL,
-  event_type int(11) NOT NULL,
-  user_id int(11) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `response`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `response` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_id` int(11) NOT NULL,
+  `event_type` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table response
+-- Dumping data for table `response`
 --
 
-LOCK TABLES response WRITE;
-/*!40000 ALTER TABLE response DISABLE KEYS */;
-INSERT INTO response VALUES (1,1,1,3);
-/*!40000 ALTER TABLE response ENABLE KEYS */;
+LOCK TABLES `response` WRITE;
+/*!40000 ALTER TABLE `response` DISABLE KEYS */;
+INSERT INTO `response` VALUES (1,1,1,3);
+/*!40000 ALTER TABLE `response` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table user
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  avatar varchar(45) DEFAULT NULL,
-  password varchar(45) NOT NULL,
-  phone varchar(15) NOT NULL,
-  username varchar(45) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY UK_589idila9li6a4arw1t8ht1gx (phone),
-  UNIQUE KEY UK_sb8bbouer5wak8vyiiy4pf2bx (username)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `avatar` varchar(45) DEFAULT NULL,
+  `password` varchar(45) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `code` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_589idila9li6a4arw1t8ht1gx` (`phone`),
+  UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table user
+-- Dumping data for table `user`
 --
 
-LOCK TABLES user WRITE;
-/*!40000 ALTER TABLE user DISABLE KEYS */;
-INSERT INTO user VALUES (3,NULL,'123456','1881925376','Gordan'),(4,NULL,'123456','123','Lin'),(5,'','123456','18819253762','GordanLin');
-/*!40000 ALTER TABLE user ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (3,NULL,'123456','1881925376','Gordan',NULL),(4,NULL,'123456','123','Lin',NULL),(6,'','123456','18819253762','guo','4111'),(10,'','bbbbbb','18826234601','aaaaaa','3450');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -200,4 +217,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-17 19:20:28
+-- Dump completed on 2017-05-27 13:54:44
